37dc14884a6f593bd8662b1193703604d9a791f7
oreohere <oyendrila.dobe@gmail.com>
2020-09-06 03:10:05 -0400

    Added 3 new files

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -28,11 +28,9 @@ class ViewController: UIViewController {
     @IBAction func CalculateTip(_ sender: Any) {
         //get the bill amount
         let bill = Double(bill_field.text!) ?? 0
-        print("hello")
         print(bill)
         //calculate tip and total
         let tip_percent = [0.1, 0.15, 0.2]
-        print("hiii")
         let tip = bill * tip_percent[tip_control.selectedSegmentIndex]
         let total = bill + tip
         
