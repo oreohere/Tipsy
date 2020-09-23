0ca561b2787688f579ad8d3a8e49641dbfbb73dc
oreohere <oyendrila.dobe@gmail.com>
2020-09-23 18:14:42 -0400

    added share option

--- Tipsy/SettingsViewController.swift
+++ Tipsy/SettingsViewController.swift
@@ -11,17 +11,52 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var settings_tip_control: UISegmentedControl!
+    @IBOutlet weak var mode_control: UISegmentedControl!
+    var flag = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        let defaults = UserDefaults.standard
+        defaults.integer(forKey: "myTipIndex")
+        let tip = defaults.integer(forKey: "tip_index")
+        print("while entering settings tip index is "+String(tip))
+        settings_tip_control.selectedSegmentIndex = tip
+        mode_control.selectedSegmentIndex = defaults.integer(forKey: "mode")
+        
+        //get data about previous screen from user default and set segmented control likewise
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+        if flag == 0{
+            let index = settings_tip_control.selectedSegmentIndex
+            let mode = mode_control.selectedSegmentIndex
+            let defaults = UserDefaults.standard
+            defaults.set(mode, forKey: "myModeIndex")
+            defaults.set(index, forKey: "myTipIndex")
+            defaults.synchronize()
+        }
+    }
     
     @IBAction func read_new_tip(_ sender: Any) {
+        flag = 1
         let index = settings_tip_control.selectedSegmentIndex
         let defaults = UserDefaults.standard
-        defaults.set(index, forKey: "myInt")
+        defaults.set(index, forKey: "myTipIndex")
         defaults.synchronize()
+    }
+    @IBAction func read_new_mode(_ sender: Any) {
+        let mode = mode_control.selectedSegmentIndex
+        let defaults = UserDefaults.standard
+        defaults.set(mode, forKey: "myModeIndex")
+        defaults.synchronize()
+        
     }
     
     /*
--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var bill_field: UITextField!
     @IBOutlet weak var tip_label: UILabel!
     @IBOutlet weak var bill_label: UILabel!
-    
     @IBOutlet weak var tip_control: UISegmentedControl!
-    
+    @IBOutlet weak var share_label: UILabel!
+    @IBOutlet weak var share_field: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("view did load")
         // Do any additional setup after loading the view.
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -26,8 +29,35 @@ class ViewController: UIViewController {
         // This is a good place to retrieve the default tip percentage from UserDefaults
         // and use it to update the tip amount
         let defaults = UserDefaults.standard
-        let index = defaults.integer(forKey: "myInt")
+        let index = defaults.integer(forKey: "myTipIndex")
         tip_control.selectedSegmentIndex = index
+        
+        let bill = Double(bill_field.text!) ?? 0
+        //calculate tip and total
+        let tip_percent = [0.1, 0.15, 0.2]
+        let tip = bill * tip_percent[tip_control.selectedSegmentIndex]
+        total = bill + tip
+        //update label with values
+        tip_label.text = String(format: "$%.2f", tip)
+        bill_label.text = String(format: "$%.2f", total)
+        share_label.text = String(format: "$%.2f", total)
+        let share_nos = Int(share_field.text!) ?? 1
+        print("share_nos"+String(share_nos))
+        let share = total / Double(share_nos)
+        share_label.text = String(format: "$%.2f", share)
+        print(share)
+        
+        let mode = defaults.integer(forKey: "myModeIndex")
+        print(mode)
+        switch mode {
+        case 0:
+            overrideUserInterfaceStyle = .light
+        case 1:
+            overrideUserInterfaceStyle = .dark
+        default:
+            overrideUserInterfaceStyle = .light
+        }
+        bill_field.becomeFirstResponder()
         
     }
 
@@ -40,6 +70,19 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         print("view will disappear")
+        let defaults = UserDefaults.standard
+        let tip_index = tip_control.selectedSegmentIndex
+        print("while exiting home tip index is "+String(tip_index))
+        var mode = 0
+        if self.traitCollection.userInterfaceStyle == .dark {
+                mode = 1
+               } else {
+                mode = 0
+               }
+        defaults.set(tip_index, forKey: "tip_index")
+        defaults.set(mode, forKey: "mode")
+        defaults.synchronize()
+        // save current screen data of segmented control to send via userdefaults
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -48,22 +91,37 @@ class ViewController: UIViewController {
     }
 
     
-    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
+    var total = 0.0
+    
     @IBAction func CalculateTip(_ sender: Any) {
         //get the bill amount
         let bill = Double(bill_field.text!) ?? 0
         //calculate tip and total
         let tip_percent = [0.1, 0.15, 0.2]
         let tip = bill * tip_percent[tip_control.selectedSegmentIndex]
-        let total = bill + tip
-        
+        total = bill + tip
         //update label with values
         tip_label.text = String(format: "$%.2f", tip)
         bill_label.text = String(format: "$%.2f", total)
+        share_label.text = String(format: "$%.2f", total)
+        let share_nos = Int(share_field.text!) ?? 1
+        print("share_nos"+String(share_nos))
+        let share = total / Double(share_nos)
+        share_label.text = String(format: "$%.2f", share)
+        print(share)
+        
         
     }
+    @IBAction func Share_calculation(_ sender: Any) {
+        let share_nos = Int(share_field.text!) ?? 1
+        print("share_nos"+String(share_nos))
+        let share = total / Double(share_nos)
+        share_label.text = String(format: "$%.2f", share)
+        print(share)
+    }
+    
 }
 
